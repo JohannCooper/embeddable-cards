d9bb76ca3771a200c2eaef5ea3d21bbb61d5d1e1
johanncooper <johann.cooper@cuepath.onmicrosoft.com>
2020-05-23 10:56:27 -0700

    mobile navbar scaling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,21 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Navigation() {
-	return (
-		<Navbar expand='lg' bg='dark' variant='dark'>
-			<Navbar.Brand href='/'>Atomic Events</Navbar.Brand>
-			<Navbar.Toggle aria-controls='basic-navbar-nav' />
-			<Navbar.Collapse id='basic-navbar-nav'>
-				<Nav activeKey={window.location.pathname} className='mr-auto'>
-					<Nav.Link href='/'>Home</Nav.Link>
-					<Nav.Link href='/cards'>Cards</Nav.Link>
-					<Nav.Link href='/chains'>Chains</Nav.Link>
-					<Nav.Link href='/feedback'>Feedback</Nav.Link>
-					<Nav.Link href='/about'>About</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
+  return (
+    <Navbar expand='lg' bg='dark' variant='dark' style={{ flex: '0 1 auto' }}>
+      <Navbar.Brand href='/'>Atomic Events</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav activeKey={window.location.pathname} className='mr-auto'>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/cards'>Cards</Nav.Link>
+          <Nav.Link href='/chains'>Chains</Nav.Link>
+          <Nav.Link href='/feedback'>Feedback</Nav.Link>
+          <Nav.Link href='/about'>About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Navigation;
