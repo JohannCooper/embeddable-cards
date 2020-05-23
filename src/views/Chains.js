db641f7c2bc5d7b96db9618d4b7f1b24ca2c402a
ssidorov <sav01027@gmail.com>
2020-05-23 01:07:22 -0600

    Created Chain mockup

--- src/views/Chains.js
+++ src/views/Chains.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import chainPng from '../images/chain-example.png';
 
 function Chains() {
-  return <div>Chains</div>;
+	return (
+		<div>
+			<h2>An example of a Chain:</h2>
+			<img src={chainPng} alt='chain-example' />
+		</div>
+	);
 }
 
 export default Chains;
