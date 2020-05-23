e291ac760387dc3061448c20c3a85f90220f5b84
johanncooper <johann.cooper@cuepath.onmicrosoft.com>
2020-05-23 12:16:20 -0700

    refactored chains

--- src/views/Chains.js
+++ src/views/Chains.js
@@ -3,28 +3,16 @@ import Card from 'react-bootstrap/Card';
 import chainPng from '../images/chain-example.png';
 
 function Chains() {
-	return (
-		<Card
-			style={{
-				maxWidth: '800px',
-				margin: '20px',
-				alignSelf: 'center',
-			}}
-		>
-			<Card.Body
-				style={{
-					display: 'flex',
-					flexFlow: 'column',
-					alignItems: 'center',
-				}}
-			>
-				<Card.Title style={{ fontWeight: 'bold' }}>
-					<i>Chains</i> are not yet implemented. Here's an example of how a <i>Chain</i> could look like:
-				</Card.Title>
-				<Card.Img src={chainPng} style={{ maxWidth: '300px', marginTop: '20px' }} />
-			</Card.Body>
-		</Card>
-	);
+  return (
+    <Card style={{ maxWidth: 800, margin: 20, alignSelf: 'center' }}>
+      <Card.Body>
+        <Card.Title style={{ fontWeight: 'bold' }}>
+          <i>Chains</i> are not yet implemented. Here's an example of how a <i>Chain</i> could look like:
+        </Card.Title>
+      </Card.Body>
+      <Card.Img src={chainPng} style={{ maxWidth: 300, margin: 'auto' }} />
+    </Card>
+  );
 }
 
 export default Chains;
