a6c333e097fb7a1c26892972a79ffcdc940ef5f9
ssidorov <sav01027@gmail.com>
2020-05-23 12:56:40 -0600

    Styled Chains page

--- src/views/Chains.js
+++ src/views/Chains.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import chainPng from '../images/chain-example.png';
 
 function Chains() {
 	return (
-		<div>
-			<h2>An example of a Chain:</h2>
-			<img src={chainPng} alt='chain-example' />
-		</div>
+		<Card
+			style={{
+				maxWidth: '800px',
+				margin: '20px',
+				alignSelf: 'center',
+			}}
+		>
+			<Card.Body
+				style={{
+					display: 'flex',
+					flexFlow: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Card.Title style={{ fontWeight: 'bold' }}>
+					<i>Chains</i> are not yet implemented. Here's an example of how a <i>Chain</i> could look like:
+				</Card.Title>
+				<Card.Img src={chainPng} style={{ maxWidth: '300px', marginTop: '20px' }} />
+			</Card.Body>
+		</Card>
 	);
 }
 
