e5131072b8bd0528f3becafefa0c8be045742706
ssidorov <sav01027@gmail.com>
2020-05-24 18:00:02 -0600

    Removed dropdown, removed Chains page, updated whitepaper.pdf

--- src/App.js
+++ src/App.js
@@ -3,35 +3,33 @@ import { Switch, Route, useLocation } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import About from './views/About';
 import Cards from './views/Cards';
-import Chains from './views/Chains';
 import Embedding from './views/Embedding';
 import Feedback from './views/Feedback';
 
 function Routes() {
-  return (
-    <Switch>
-      <Route exact path='/' component={Cards} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/cards' component={Cards} />
-      <Route exact path='/chains' component={Chains} />
-      <Route path='/embed/:id' component={Embedding} />
-      <Route exact path='/feedback' component={Feedback} />
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route exact path='/' component={Cards} />
+			<Route exact path='/about' component={About} />
+			<Route exact path='/cards' component={Cards} />
+			<Route path='/embed/:id' component={Embedding} />
+			<Route exact path='/feedback' component={Feedback} />
+		</Switch>
+	);
 }
 
 function App() {
-  const location = useLocation();
-  const routeIndex = location.pathname.search(/\w\/.*$/g);
-  const src = location.pathname.slice(routeIndex + 1);
-  const realLocation = location.pathname.replace(src, '');
+	const location = useLocation();
+	const routeIndex = location.pathname.search(/\w\/.*$/g);
+	const src = location.pathname.slice(routeIndex + 1);
+	const realLocation = location.pathname.replace(src, '');
 
-  return (
-    <div style={{ display: 'flex', flexFlow: 'column', height: '100%' }}>
-      {realLocation !== '/embed' ? <Navigation /> : null}
-      <Routes />
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex', flexFlow: 'column', height: '100%' }}>
+			{realLocation !== '/embed' ? <Navigation /> : null}
+			<Routes />
+		</div>
+	);
 }
 
 export default App;
